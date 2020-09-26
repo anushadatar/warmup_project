dd7c019597aedb668429638612dd844a8101e44d
Anusha Datar <anushadatar@gmail.com>
2020-09-26 12:08:41 -0400

    added comments to drive_square_timing

--- warmup_project/scripts/drive_square_timing.py
+++ warmup_project/scripts/drive_square_timing.py
@@ -4,26 +4,27 @@ import time
 
 from geometry_msgs.msg import Twist, Vector3
 
-
 class Drive_Square_Node():
     """
-    Node associated with driving the Neato in a square.
+    TODO: Drive in a square using /odom as well.
+    Node that drives the Neato in a square using timing.
     """
     def __init__(self):
         """
-        Kick off square driving node and assocaited cmd_vel publisher.
+        Initialize ROS and directional parameters.
         """
+        # ROS Parameters.
         rospy.init_node("Drive_Square")
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         
-        # Define directional messages.
+        # Directional messages.
         self.neato_turn = Twist(Vector3(0, 0, 0), Vector3(0, 0, 1))
         self.neato_stop = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
         self.neato_forward = Twist(Vector3(1, 0, 0), Vector3(0, 0, 0))
     
     def turn_neato(self):
         """
-        Stop, move forward, and turn the neato.
+        Publish velocity messages to stop, move forward, and then turn the neato.
         """
         self.pub.publish(self.neato_stop)
         rospy.sleep(2)
@@ -33,6 +34,9 @@ class Drive_Square_Node():
         rospy.sleep(2)
 
     def run(self):
+        """
+        Turns the neato 4 times to execute on driving in a square.
+        """
         for i in range(4):
             self.turn_neato()
 
